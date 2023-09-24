b5ff784f403b892117eaefc13ecd97e236af82d5
Demian <demyan.korotchenko@gmail.com>
2023-09-24 10:41:44 +0200

    add_redux

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+import { Grid, GridItem } from './Grid/Grid.styled';
+import { Todo } from './Todo/Todo';
+import { selectTodos } from 'redux/selector';
+
+export const TodoList = () => {
+  const todos = useSelector(selectTodos);
+  return (
+    <Grid>
+      {todos.map((todo, index) => (
+        <GridItem key={todo.id}>
+          <Todo id={todo.id} text={todo.text} counter={index + 1} />
+        </GridItem>
+      ))}
+    </Grid>
+  );
+};
